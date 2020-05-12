e426e18147c69644089d8cea1e2687427e9245f7
Gianniii <Gianni.lodetti@hotmail.com>
2020-05-12 18:19:56 +0200

    modified terrain slightly to look infinit

--- src/main_terrain.js
+++ src/main_terrain.js
@@ -314,7 +314,7 @@ async function main() {
 				100, // far
 			)
 
-			texture_fbm.draw_texture_to_buffer({width: 500, height: 300, mouse_offset, zoom_factor: 2.5});
+			texture_fbm.draw_texture_to_buffer({width: 500, height: 300, mouse_offset, zoom_factor: 2.});
 			//texture_fbm.draw_buffer_to_screen();
 
 			mat4.copy(mat_view, mat_world_to_cam);
